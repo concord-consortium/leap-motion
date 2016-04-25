e11550cf0b00b359c211c7370f5b1d028555c0ba
Piotr Janik <janikpiotrek@gmail.com>
2016-04-25 16:12:19 +0200

    Move the Leap hand view to a transparent overlay
    [#118212653]
    [#118216579]

--- webpack.config.js
+++ webpack.config.js
@@ -21,7 +21,8 @@ module.exports = {
   module: {
     loaders: [
       { test: /\.jsx?$/, exclude: /node_modules/, loader: 'babel-loader?optional=runtime' },
-      { test: /\.css$/, exclude: /node_modules/, loader: 'style-loader!css-loader' },
+      { test: /\.css$/, exclude: /node_modules/, loader: 'style!css!autoprefixer' },
+      { test: /\.less$/, exclude: /node_modules/, loader: 'style!css!less!autoprefixer' },
       { test: /\.json$/, exclude: /node_modules/, loader: 'json-loader' },
       { test: /\.(png|jpg|wav)$/, exclude: /node_modules/, loader: 'file-loader' }
     ]
