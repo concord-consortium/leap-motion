b5f10ba47d9fcf5f383416520d5f0ff5671a6a55
Piotr Janik <janikpiotrek@gmail.com>
2016-04-25 15:41:25 +0200

    Reorganize Leap status components
    [#118212653]

--- apps/common/js/components/leap-standard-info.jsx
+++ apps/common/js/components/leap-standard-info.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
 import LeapHandsView from './leap-hands-view.jsx';
-import LeapFrameRate from './leap-frame-rate.jsx';
-import Plotter from './plotter.jsx';
+import LeapStatus from './leap-status.jsx';
+import '../../css/leap-standard-info.css';
 
 export default class LeapStandardInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      debugEnabled: true
-    };
-    this._handleDebugChange = this._handleDebugChange.bind(this);
-  }
-
   get plotter() {
-    return this.refs.plotter;
-  }
-
-  _handleDebugChange(event) {
-    this.setState({debugEnabled: event.target.checked});
+    return this.refs.status.plotter;
   }
 
   render() {
@@ -25,11 +13,7 @@ export default class LeapStandardInfo extends React.Component {
       <div className='leap-standard-info'>
         <div className='state-and-plotter'>
           <div className='state-msg'>{this.props.stateMsg}</div>
-          <div className='status-box'>
-            <label><input type='checkbox' checked={this.state.debugEnabled} onChange={this._handleDebugChange}/> Status</label>
-            {this.state.debugEnabled ? <LeapFrameRate/> : ''}
-            <Plotter ref='plotter' hidden={!this.state.debugEnabled}/>
-          </div>
+          <LeapStatus ref='status'/>
         </div>
         <LeapHandsView height='347'/>
       </div>
