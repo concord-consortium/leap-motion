8e9845fbd7ab8e49d86e1db35849f3bb96e4f92e
Piotr Janik <janikpiotrek@gmail.com>
2016-06-01 15:32:20 +0200

    Add movie "help" to beginning of gas model overlay
    [#120278309]

--- apps/common/js/components/instructions-overlay.jsx
+++ apps/common/js/components/instructions-overlay.jsx
@@ -5,10 +5,10 @@ import '../../css/instructions-overlay.less';
 export default class InstructionsOverlay extends React.Component {
 
   render() {
-    const { visible, width, height, children } = this.props;
+    const { visible, width, height, className, children } = this.props;
 
     return (
-      <div className={`overlay ${visible ? '' : 'hidden'}`}>
+      <div className={`overlay ${className} ${visible ? '' : 'hidden'}`}>
         <LeapHandsView width={width} height={height}/>
         <div className='instructions'>
           {children}
