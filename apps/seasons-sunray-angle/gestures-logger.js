64d66921ee27a06b74bca056927a44501b8d575c
Piotr Janik <janikpiotrek@gmail.com>
2016-11-02 18:01:33 +0100

    Log hands interaction statistics
    [#132351255]

--- apps/seasons-sunray-angle/gestures-logger.js
+++ apps/seasons-sunray-angle/gestures-logger.js
@@ -8,13 +8,16 @@ export default class GesturesLogger {
     this._prevGestureTimestamp = null;
     this._prevGesture = null;
     this._prevDay = null;
+    this._stats = {};
+    this._statsTimestamp = null;
   }
 
   logGesture(data, gestureDetected, currentDay) {
+    const now = performance.now();
     let newGesture = null;
     if (gestureDetected && data.numberOfHands === 1) {
       newGesture = 'Gesture:SetAngle';
-    } else if (gestureDetected && data.numberOfHands === 1) {
+    } else if (gestureDetected && data.numberOfHands === 2) {
       newGesture = 'Gesture:SetDistance';
     } else if (data.numberOfHands === 1 && data.handType === 'left') {
       newGesture = 'Gesture:OneHandNotAligned';
@@ -25,8 +28,14 @@ export default class GesturesLogger {
     } else if (data.numberOfHands === 2 && data.handsVertical) {
       newGesture = 'Gesture:TwoHandsNotAligned';
     }
+    // Setup stats when some gesture is detected (and there were no hands visible before).
+    if (this._prevGesture === null && newGesture !== null) {
+      this._stats = {};
+      this._statsTimestamp = now;
+    }
+    // Collect statistics on single gesture and try to log them.
     if (this._prevGesture && this._prevGesture !== newGesture) {
-      const duration = (Date.now() - this._prevGestureTimestamp) / 1000;
+      const duration = (now - this._prevGestureTimestamp) / 1000;
       if (duration > MIN_GESTURE_DURATION) {
         logger.log(this._prevGesture, {
           duration,
@@ -34,11 +43,27 @@ export default class GesturesLogger {
           prevDay: this._prevDay
         });
       }
+      if (!this._stats[this._prevGesture]) {
+        this._stats[this._prevGesture] = 0;
+      }
+      this._stats[this._prevGesture] += duration;
       this._prevGestureTimestamp = null;
     }
+    // Log statistic when hands interaction is finished.
+    if (this._prevGesture !== null && newGesture === null) {
+      const duration = (now - this._statsTimestamp) / 1000;
+      Object.keys(this._stats).forEach(key => {
+        this._stats[key] = (this._stats[key] / duration) * 100;
+      });
+      logger.log('HandsInteractionSummary', {
+        duration,
+        stats: this._stats
+      });
+    }
+
     this._prevGesture = newGesture;
     if (this._prevGestureTimestamp === null && newGesture !== null) {
-      this._prevGestureTimestamp = Date.now();
+      this._prevGestureTimestamp = now;
       this._prevDay = currentDay;
     }
   }
