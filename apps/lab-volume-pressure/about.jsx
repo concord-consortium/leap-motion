bcf5ccd5e0542b5fd9196d70acb3c0144c1ef2ef
Chris Hart <chart@concord.org>
2017-07-13 11:56:27 -0400

    Update About text wording.

--- apps/lab-volume-pressure/about.jsx
+++ apps/lab-volume-pressure/about.jsx
@@ -6,7 +6,7 @@ export default () => {
       <h3>Gas Pressure vs. Volume</h3>
 
       <p>
-        Explore the relationship of pressure of a gas to its volume using a Leap Motion controller attached to the computer.
+        Explore the relationship of pressure of a gas to its volume using a Leap Motion Controller attached to the computer.
       </p><p>
         Gases contained in a syringe can be compressed into smaller volumes. Following the prompts on screen, use the motion of your hands to change the pressure of the molecules of the gas. The closed fist of one hand represents the motion of the molecules. The flat hand represents the surface of the syringe plunger. What do the red squares that appear on the syringe plunger represent?
       </p>
