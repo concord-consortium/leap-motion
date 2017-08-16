5379c53852e949da61559b9ccc74331d1f5dc788
Chris Hart <chart@concord.org>
2017-08-16 10:51:03 -0400

    Tidy up some of the styling for regular and narrow layout

--- apps/seasons-sunray-angle/seasons-sunray-angle.jsx
+++ apps/seasons-sunray-angle/seasons-sunray-angle.jsx
@@ -291,7 +291,7 @@ export default class SeasonsSunrayAngle extends React.Component {
   render() {
     const { instructions, activeViewPanel, overlayEnabled, overlayActive, phantomHandsHint, renderSize } = this.state;
     let overlaySizeSettings = renderSize == 'seasons' ? OVERLAY_SIZE : OVERLAY_SIZE_NARROW;
-    let containerStyle = renderSize == 'seasons' ? 'seasons-container' : 'seasons-container-narrow';
+    let containerStyle = renderSize == 'seasons' ? 'seasons-container' : 'seasons-container narrow';
     // Each time user changes position of the rays view, we need to reposition and resize overlay.
     // Position is updated using CSS styles (set by class name, see seasons-sunray-angle.less).
     // Width and height need to be set using React properties, so overlay component can resize its 3D renderer.
